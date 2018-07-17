da4b77a5e2db973d37d3946fc0e76a7e5f4b19b3
beeMonkey <675383749@qq.com>
2018-07-18 01:59:24 +0800

    change the check function

--- js/todoMVC.js
+++ js/todoMVC.js
@@ -1,7 +1,7 @@
 $(document)
     .ready(function () {
         const todoForm = {
-            todos: [{id:"zuixian",name:"默认第一任务"},{name:"可选的"}],
+            todos: [{id:"zuixian",name:"默认第一任务",complete:false},{id:"23",name:"可选的",complete:false}],
             statusOfList: "all"
         }
 
@@ -11,9 +11,14 @@ $(document)
         
         renderTodoList=(todos)=>{
             return todos.map(todo=>{
-                return `<li id=${todo.id} class="">
-                <input name="done-todo" type="checkbox" class="done-todo"> ${todo.name} </li>`
-            }).join("");
+                // return `<li id=${todo.id} class="">
+                // <input name="done-todo" type="checkbox" class="done-todo"> ${todo.name} </li>`
+                return `<li class="${todo.complete ? "checked" : ""}" 
+                ondblclick="editItem(event, '${todo.id}')">
+                    <input name="done-todo" ${todo.complete ? 'checked' : ""} type="checkbox" class="done-todo" onchange="checkItem('${todo.id}')"/> 
+                    ${todo.name}
+                 </li>`
+            }).join("");    
         }
 
         window.addItem=()=>{
@@ -22,7 +27,35 @@ $(document)
             render();
         }
 
+        window.checkItem = (viewId) => {
+            let checkedItem = todoForm.todos.find(item => item.id === viewId)
 
+            if(checkedItem !== undefined){
+                checkedItem.complete = !checkedItem.complete;
+            }
+            render();
+        }
+
+        window.showTodoList=(filterType)=>{
+            todoForm.statusOfList =filterType;
+            render();
+        }
+
+        window.editItem = (event, viewId) => {
+            $(event.target).attr('contentEditable', 'true')
+                .focus()
+                .keypress(function (event) {
+                    var keycode = (event.keyCode
+                        ? event.keyCode
+                        : event.which);
+                       
+                    if (keycode == '13') {
+                        todoForm.todos.find(element => element.id === viewId).name = $(event.target).text();
+                        render();
+                    }
+
+                })
+        }
         const buildHTML=(todoForm)=> {
             return `
             <div>
@@ -37,7 +70,8 @@ $(document)
                 <a href="#" data-filter="all" class="selected">ALL</a>
             </li>
             <li>
-                <a href="#" data-filter="active" class="">Active</a>
+                <a href="#" data-filter="active" class="${todoForm.statusOfList == "active" ? "selected" : ""}" 
+                onclick="showTodoList('active')">Active</a>
             </li>
             <li>
                 <a href="#" data-filter="complete" class="">Complete</a>
@@ -72,30 +106,33 @@ $(document)
             return uuid;
         }
 
-        $("body").on("click", ".done-todo", function () {
-            if ($(this).parent().hasClass("checked")) {
-                $(this).parent().removeClass("checked");
-            } else {
-                $(this).parent().addClass("checked");
-            }
-        });
+        // $("body").on("click", ".done-todo", function () {
+        //     if ($(this).parent().hasClass("checked")) {
+        //         $(this).parent().removeClass("checked");
+        //     } else {
+        //         $(this).parent().addClass("checked");
+        //     }
+        // });
 
-        $("ol li").click(function () {
-            $(this).text.attr("contentEditable", true);
-        })
+        // $("ol li").click(function () {
+        //     $(this).attr("contentEditable", true);
+        // })
 
-        $("a[data-filter=all]").click(function () {
-            $("ol li").css("display", "");
-        });
+        // $("a[data-filter=all]").click(function () {
+        //     $("ol li").css("display", "");
+        // });
 
-        $("a[data-filter=active]").click(function () {
+        // $("a[data-filter=active]").click(function () {
+        //     $("ol").children().filter('.checked').css("display", "none")
+        //     $("ol").children().not('.checked').css("display", "")
+        // });
+        $("body").on("clcik","a[data-filter=active]",function(){
             $("ol").children().filter('.checked').css("display", "none")
             $("ol").children().not('.checked').css("display", "")
-        });
-
-        $("a[data-filter=complete]").click(function () {
-            $("ol").children().not('.checked').css("display", "none")
-            $("ol").children().filter('.checked').css("display", "")
-        });
+        })
+        // $("a[data-filter=complete]").click(function () {
+        //     $("ol").children().not('.checked').css("display", "none")
+        //     $("ol").children().filter('.checked').css("display", "")
+        // });
 
     });
